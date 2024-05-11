a85db2af2b1ab192fd7de488a0f54b24e16033ac
Netcode-Hub <Netcode-Hub@DESKTOP-ULDGKTU>
2024-05-11 16:01:54 +0000

    Add project files.

---
+++ DemoAPIVersioning/Controllers/v3/WeatherForecastController.cs
@@ -0,0 +1,36 @@
+using Asp.Versioning;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoAPIVersioning.Controllers.v3
+{
+    [ApiController]
+    [ApiVersion(3)]
+    //[Route("api/[controller]")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("range/{number}")]
+        public IEnumerable<WeatherForecast> Get(int number)
+        {
+            return Enumerable.Range(1, number).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
